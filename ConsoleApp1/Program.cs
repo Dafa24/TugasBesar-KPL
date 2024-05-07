c9b4b2ca8672e0a9919153ccc5a3ae8ab9e74ed8
IrjiSyahrul <irjisyahrukchannel@gmail.com>
2024-05-07 11:58:12 +0700

    TableDriven

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using static ConsoleApp1.TableDriven;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+    
+            TabelInventaris inventaris = new TabelInventaris();
+
+            // Menambahkan barang dengan menggunakan enum untuk jenis dan status
+            inventaris.TambahBarang("Keyboard", "Keyboard Mechanic", 50, JenisBarang.Elektronik);
+            inventaris.TambahBarang("Mouse", "Gaming Mouse", 30, JenisBarang.Elektronik);
+
+        // Contoh penggunaan: Menampilkan barang dengan ID 1
+        Barang barang = inventaris.GetBarang(1);
+        if (barang != null)
+        {
+            Console.WriteLine($"Barang ditemukan: {barang.Nama}, Deskripsi: {barang.Deskripsi}, Harga: {barang.Harga}");
+        }
+        else
+        {
+            Console.WriteLine("Barang tidak ditemukan.");
+        }
+    }
+    }
